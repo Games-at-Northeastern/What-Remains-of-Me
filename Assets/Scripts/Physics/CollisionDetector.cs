d5968b82d04de6ea0f714535338040d3ba3916c8
Connor Saila <csaila@live.com>
2022-10-10 11:16:38 -0400

    Updated CollisionDetector.cs and Player prefab colliders
    Updated comment formatting in CollisionDetector.cs and also changed it to work with all Collider2D objects as opposed to only BoxCollider2D. Also updated Player prefab collider to be a capsule and the "Ground Detector" to better represent the sprite to prevent clipping on ledges.

--- Assets/Scripts/Physics/CollisionDetector.cs
+++ Assets/Scripts/Physics/CollisionDetector.cs
@@ -5,39 +5,41 @@ using UnityEngine;
 /// <summary>
 /// Class for giving whether or not this gameObject is colliding
 /// with a certain set of layers.
-///
-/// At the moment, only works with box colliders, which should be good
-/// enough for most purposes.
 /// </summary>
 [RequireComponent(typeof(Collider2D))]
 public class CollisionDetector : MonoBehaviour
 {
-    BoxCollider2D col;
+    Collider2D col;
     [SerializeField] LayerMask collidesWith;
     [SerializeField] bool collidesWithTriggers;
 
+    /**
+     * Initializes col to the attached object's collider.
+     */
     private void Awake()
     {
-        col = GetComponent<BoxCollider2D>();
+        col = GetComponent<Collider2D>();
     }
 
-    /// <summary>
-    /// Is the collider for this gameObject touching any of the layers
-    /// in the provided mask?
-    /// </summary>
+    /**
+     * Is the collider for this gameObject touching any of the layers 
+     * in the provided mask?
+     */
     public bool isColliding()
     {
         return col.IsTouchingLayers(collidesWith);
     }
 
-    /// <summary>
-    /// What gameObject is this gameObject colliding with? If multiple, chooses
-    /// any one.
-    /// </summary>
+    /**
+     * What gameObject is this gameObject colliding with? If multiple, chooses
+     * any one.
+     */
     public GameObject CollidingWith()
     {
-        Collider2D collidingCol = Physics2D.OverlapArea(col.bounds.min, col.bounds.max, collidesWith);
-        bool invalidCol = (collidingCol == null) || (!collidesWithTriggers && collidingCol.isTrigger);
+        Collider2D collidingCol = Physics2D.OverlapArea(col.bounds.min, col.bounds.max, 
+                                                        collidesWith);
+        bool invalidCol = (collidingCol == null) 
+            || (!collidesWithTriggers && collidingCol.isTrigger);
         return invalidCol ? null : collidingCol.gameObject;
     }
 }
