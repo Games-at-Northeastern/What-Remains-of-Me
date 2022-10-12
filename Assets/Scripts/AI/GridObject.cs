5ffd289458fb155ba0c64525301a85b0ebf0befc
mike-935 <Michael935935@gmail.com>
2022-10-12 01:52:43 -0400

    Comments added to AI folder

--- Assets/Scripts/AI/GridObject.cs
+++ Assets/Scripts/AI/GridObject.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class that represents a Grid to be used for AI pathing
+/// </summary>
 public class GridObject : MonoBehaviour
 {
     public int width;
@@ -10,11 +13,18 @@ public class GridObject : MonoBehaviour
 
     private PathfindingGrid grid;
 
+    /// <summary>
+    /// Initialize a Pathing Grid.
+    /// </summary>
     void Start()
     {
         grid = new PathfindingGrid(width, height, cellSize, this.transform.position);
     }
 
+    /// <summary>
+    /// Returns the Pathing Grid.
+    /// </summary>
+    /// <returns></returns>
     public PathfindingGrid GetPathfindingGrid()
     {
         return grid;
--- Assets/Scripts/AI/PathfindingMinheap.cs
+++ Assets/Scripts/AI/PathfindingMinheap.cs
@@ -3,20 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class that represents finding the minimum heap path
+/// </summary>
 public class PathfindingMinheap
 {
     private List<Tuple<Vector3Int, float>> heap;
 
+    /// <summary>
+    /// Initialize heap to be a new list tuple of Vector3Int type float
+    /// </summary>
     public PathfindingMinheap()
     {
         heap = new List<Tuple<Vector3Int, float>>();
     }
 
+    /// <summary>
+    /// Adds a given Vector3Int to the heap with a given priority
+    /// </summary>
+    /// <param name="pos">Vector3Int to add</param>
+    /// <param name="priority">Its priority</param>
     public void Add(Vector3Int pos, float priority)
     {
         heap.Add(new Tuple<Vector3Int, float>(pos, priority));
     }
 
+    /// <summary>
+    /// Gets the Top tuple in the heap
+    /// </summary>
+    /// <returns>The Top tuple</returns>
     private Tuple<Vector3Int, float> GetTopTuple()
     {
         Tuple<Vector3Int, float> bottom = new Tuple<Vector3Int, float>(new Vector3Int(-9999, -9999, -9999), 9999);
@@ -30,11 +45,19 @@ public class PathfindingMinheap
         return bottom;
     }
 
+    /// <summary>
+    /// Gets the first component of the TopTuple
+    /// </summary>
+    /// <returns>the first component from the TopTuple</returns>
     public Vector3Int Top()
     {
         return GetTopTuple().Item1;
     }
 
+    /// <summary>
+    /// Pop's off the bottom element from TopTuple and returns it
+    /// </summary>
+    /// <returns>Bottom element</returns>
     public Vector3Int Pop()
     {
         Tuple<Vector3Int, float> bottom = GetTopTuple();
@@ -43,6 +66,10 @@ public class PathfindingMinheap
         return bottomPos;
     }
 
+    /// <summary>
+    /// Checks if the heap is currently empty
+    /// </summary>
+    /// <returns>If the heap is empty</returns>
     public bool IsEmpty()
     {
         return heap.Count == 0;
