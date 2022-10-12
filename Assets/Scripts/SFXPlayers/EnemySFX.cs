4d13a75a9cc10e65c67cae7e09a8647818cc38c6
lennieok <elena.o.kosowski@gmail.com>
2022-10-11 20:03:01 -0400

    Update EnemySFX.cs

--- Assets/Scripts/SFXPlayers/EnemySFX.cs
+++ Assets/Scripts/SFXPlayers/EnemySFX.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+/// <summary>
+/// Plays a desired clip when an action is performed by the player
+/// </summary>
 public class EnemySFX : MonoBehaviour
 {
     public AudioSource src;
@@ -10,18 +15,27 @@ public class EnemySFX : MonoBehaviour
     public AudioClip overloaded;
     public AudioClip drained;
 
+    /**
+        * Plays the audio clip for walking
+    **/
     void Walk()
     {
         src.clip = walk;
         src.Play();
     }
 
+    /**
+        * Plays the audio clip for attacking
+    **/
     void Attack()
     {
         src.clip = attack;
         src.Play();
     }
 
+    /**
+        * Plays the audio clip for being overloaded
+    **/
     void OverLoaded()
     {
         src.clip = overloaded;
