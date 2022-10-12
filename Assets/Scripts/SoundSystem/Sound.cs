a82885158a7a3e4cae7cf06ae4d8425560dae744
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2022-10-12 00:06:52 -0400

    Update SoundController.cs and Sound.cs
    - Sound.cs is now a scriptable object that will be created in the asset menu to avoid the editor loosing sound settings
    
    - SoundController.cs was made more efficient by removing extra AudioSources. Doing so has drawbacks, however. Please read :
    https://trello.com/c/rY7CpYpN/891-2-refactor-soundcontrollercs-to-use-playoneshot-instead-of-multiple-sources

--- Assets/Scripts/SoundSystem/Sound.cs
+++ Assets/Scripts/SoundSystem/Sound.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 //Represents a single sound effect, a song, or a layer in a layered song/sound. Used in tandem with the SoundController.cs
 //class. 
-[System.Serializable]
-public class Sound
+[CreateAssetMenu(fileName = "New Sound", menuName = "Sound Effects")]
+public class Sound : ScriptableObject
 {
-    public string name; // The name of the sound to be called in any of the SoundController functions.
+    public string soundName; // The name of the sound to be called in any of the SoundController functions.
 
     public AudioClip clip; // The clip itself (.wav, .mp3, etc.) 
 
     [Range(0, 1.5f)]
-    public float baseVolume = 1; // Base volume of the clip (can be lowered by fade in/out)
+    public float baseVolume = 1; // Base volume of the clip (can be lowered by fade in/out, but this value will not change.)
     [Range(0.2f, 3f)]
-    public float pitch = 1;
+    public float basePitch = 1;
 
     public bool loop; // Should this sound loop itself? 
 
