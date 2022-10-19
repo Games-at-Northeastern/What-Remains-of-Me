22dd5b0bb0671ac5516cdac353149f3c58cb35a6
mike-935 <Michael935935@gmail.com>
2022-10-19 00:27:43 -0400

    Reformat Scripts
    Not much to do but changed some formatting and modified comments where it was fit.

--- Assets/Scripts/SFXPlayers/EnemySFX.cs
+++ Assets/Scripts/SFXPlayers/EnemySFX.cs
@@ -6,38 +6,34 @@ using UnityEngine;
 /// Plays the designated audio clip for the enemy's actions
 /// <summary>
 
-public class EnemySFX : MonoBehaviour
-{
-    public AudioSource src;
-    public AudioClip walk;
-    public AudioClip attack;
-    public AudioClip overloaded;
-    public AudioClip drained;
+public class EnemySFX : MonoBehaviour {
+   public AudioSource src;
+   public AudioClip walk;
+   public AudioClip attack;
+   public AudioClip overloaded;
+   public AudioClip drained;
 
-    /// <summary>
-    /// Plays the walking audio clip at the enemy's location
-    /// <summary>
-    void Walk()
-    {
-        src.clip = walk;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the walking audio clip at the enemy's location
+   /// <summary>
+   void Walk() {
+      src.clip = walk;
+      src.Play();
+   }
 
-    /// <summary>
-    /// Plays the attacking audio clip at the enemy's location
-    /// <summary>
-    void Attack()
-    {
-        src.clip = attack;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the attacking audio clip at the enemy's location
+   /// <summary>
+   void Attack() {
+      src.clip = attack;
+      src.Play();
+   }
 
-    /// <summary>
-    /// Plays the overloaded audio clip at the enemy's location
-    /// <summary>
-    void OverLoaded()
-    {
-        src.clip = overloaded;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the overloaded audio clip at the enemy's location
+   /// <summary>
+   void OverLoaded() {
+      src.clip = overloaded;
+      src.Play();
+   }
 }
--- Assets/Scripts/SFXPlayers/PlayerSFX.cs
+++ Assets/Scripts/SFXPlayers/PlayerSFX.cs
@@ -6,68 +6,61 @@ using UnityEngine;
 /// <summary>
 /// Plays the designated audio clip for the player's actions
 /// <summary>
-public class PlayerSFX : MonoBehaviour
-{
+public class PlayerSFX : MonoBehaviour {
 
-    public AudioSource src;
-    public AudioClip walkLeftFoot;
-    public AudioClip walkRightFoot;
-    public AudioClip jumpUp;
-    // public AudioClip jumpLand;
-    public AudioClip swing;
-    public AudioClip damaged;
+   public AudioSource src;
+   public AudioClip walkLeftFoot;
+   public AudioClip walkRightFoot;
+   public AudioClip jumpUp;
+   // public AudioClip jumpLand;
+   public AudioClip swing;
+   public AudioClip damaged;
 
-    /// <summary>
-    /// Plays the left foot's walking audio clip at the player's location
-    /// <summary>
-    void WalkLeftFoot()
-    {
-        src.clip = walkLeftFoot;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the left foot's walking audio clip at the player's location
+   /// <summary>
+   void WalkLeftFoot() {
+      src.clip = walkLeftFoot;
+      src.Play();
+   }
 
-    /// <summary>
-    /// Plays the right foot's walking audio clip at the player's location
-    /// <summary>
-    void WalkRightFoot()
-    {
-        src.clip = walkRightFoot;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the right foot's walking audio clip at the player's location
+   /// <summary>
+   void WalkRightFoot() {
+      src.clip = walkRightFoot;
+      src.Play();
+   }
 
-    /// <summary>
-    /// Plays the jumping audio clip at the player's location
-    /// <summary>
-    void JumpUp()
-    {
-        src.clip = jumpUp;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the jumping audio clip at the player's location
+   /// <summary>
+   void JumpUp() {
+      src.clip = jumpUp;
+      src.Play();
+   }
 
-    /*
-    void JumpLand()
-    {
-        src.clip = jumpLand;
-        src.Play();
-    }
-    */
+   /*
+   void JumpLand() {
+      src.clip = jumpLand;
+      src.Play();
+   }
+   */
 
-    /// <summary>
-    /// Plays the swinging audio clip at the player's location
-    /// <summary>
-    void Swing()
-    {
-        src.clip = swing;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the swinging audio clip at the player's location
+   /// <summary>
+   void Swing() {
+      src.clip = swing;
+      src.Play();
+   }
 
-    /// <summary>
-    /// Plays the damaged audio clip at the player's location
-    /// <summary>
-    void Damaged()
-    {
-        src.clip = damaged;
-        src.Play();
-    }
+   /// <summary>
+   /// Plays the damaged audio clip at the player's location
+   /// <summary>
+   void Damaged() {
+      src.clip = damaged;
+      src.Play();
+   }
 
 }
