cefa49b2f2f1ad22bb86def1746ddb97138bce48
melodicht <marvincht@gmail.com>
2022-11-29 10:43:44 -0500

    Fix parallax background and underwater background resolution

--- Assets/Scripts/ParallaxBG.cs
+++ Assets/Scripts/ParallaxBG.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxBG : MonoBehaviour
 {
+    private Transform _cameraTransform;
+
     private List<GameObject> backgroundElements;
 
     public float elementSize;
@@ -11,9 +14,23 @@ public class ParallaxBG : MonoBehaviour
     public GameObject referenceObject;
     public float moveSpeed;
 
+    [SerializeField] private Vector2 _parallaxEffectMuliplier;
+
+
+    private Vector3 _lastCameraPosition;
+    private Vector3 _deltaMovement;
+    private Vector3 _newCameraPosition;
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+        _lastCameraPosition = _cameraTransform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        /*
         backgroundElements = new List<GameObject>();
 
         // getting number of children objects under the backgroundElements parent object
@@ -21,7 +38,19 @@ public class ParallaxBG : MonoBehaviour
         {
             backgroundElements.Add(transform.GetChild(i).gameObject);
         }
+        */
     }
+
+    private void Update()
+    {
+        _newCameraPosition = _cameraTransform.position;
+        _deltaMovement = _newCameraPosition - _lastCameraPosition;
+        transform.position -= new Vector3(_deltaMovement.x * _parallaxEffectMuliplier.x,
+            _deltaMovement.y * _parallaxEffectMuliplier.y);
+        _lastCameraPosition = _newCameraPosition;
+    }
+
+    /*
 
     // Update is called once per frame
     void Update()
@@ -44,4 +73,5 @@ public class ParallaxBG : MonoBehaviour
             backgroundElement.transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         }
     }
+    */
 }
