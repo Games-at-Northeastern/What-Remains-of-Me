7e2e0d53d7f12c382200c3dd052fcb8c4ecd526f
Bradley Karmin <44073701+Bruggles718@users.noreply.github.com>
2022-11-30 18:30:50 -0500

    Converted Dialogue Scriptable Object system to one that uses JSON text files
    Added the ability to give a filename for the dialogue scriptable object to read from in order to allow for writing dialogue in a JSON format.

--- Assets/Scripts/Dialogue/New Dialogue/DialogueSO.cs
+++ Assets/Scripts/Dialogue/New Dialogue/DialogueSO.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
 
 [CreateAssetMenu]
 public class DialogueSO : ScriptableObject
 {
+    [Tooltip("The name of the text file that this object will be stored in. Do not write file-type extension.")]
+    [SerializeField] private string filename;
+
     //the name of the character to be displayed
     [SerializeField] private string name;
 
@@ -11,7 +17,78 @@ public class DialogueSO : ScriptableObject
     [TextArea(3, 10)]
     [SerializeField] private string[] sentences;
 
-    public string Name => name;
+    public string Name => GetName();
 
-    public string[] Sentences => sentences;
+    public string[] Sentences => GetSentences();
+
+
+    /// <summary>
+    /// reads the sentences for this dialogue object from the specified json text file
+    /// </summary>
+    /// <returns>the sentences as an array of strings</returns>
+    private string[] GetSentences()
+    {
+        this.LoadFromFile(this.filename + ".txt", out var result);
+        var jsonDialogue = new JsonDialogue();
+        JsonUtility.FromJsonOverwrite(result, jsonDialogue);
+        return jsonDialogue.Sentences;
+    }
+
+    /// <summary>
+    /// reads the name for this dialogue object from the specified json text file
+    /// </summary>
+    /// <returns>the name as a string</returns>
+    private string GetName()
+    {
+        this.LoadFromFile(this.filename + ".txt", out var result);
+        var jsonDialogue = new JsonDialogue();
+        JsonUtility.FromJsonOverwrite(result, jsonDialogue);
+        return jsonDialogue.Name;
+    }
+
+    /// <summary>
+    /// Loads the JSON text file to a result string
+    /// </summary>
+    /// <param name="fileName">The name of the file to be loaded</param>
+    /// <param name="result">the string to load the json to</param>
+    /// <returns>true if the file is successfully loaded, false if it needs to be created before loading</returns>
+    private bool LoadFromFile(string fileName, out string result)
+    {
+        var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+
+        try
+        {
+            result = File.ReadAllText(fullPath);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Failed to read from {fullPath} with exception {e}");
+            File.WriteAllText(fullPath, JsonUtility.ToJson(new JsonDialogue(this.name, this.sentences)));
+            result = File.ReadAllText(fullPath);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Serializable class to store the information of a Dialogue Scriptable Object in a text file in a JSON format.
+    /// </summary>
+    [Serializable]
+    private class JsonDialogue
+    {
+        public string Name;
+        public string[] Sentences;
+
+        public JsonDialogue()
+        {
+            Name = "";
+            Sentences = new string[] { };
+        }
+
+        public JsonDialogue(string name, string[] sentences)
+        {
+            Name = name;
+            Sentences = sentences;
+        }
+    }
 }
