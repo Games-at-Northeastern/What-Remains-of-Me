987d61956c3a4da566e0bf50c6768a0fa6dc5ebe
mkblumenau <98577204+mkblumenau@users.noreply.github.com>
2023-03-18 08:19:52 -0400

    Adding the death sound
    I previously committed this to S23-5 by mistake. This commit adds the player death sound to the demo. For now, that just means having it play when the Spike Teleport event happens. To do this I also made the functions in the PlayerSFX script public.

--- Assets/Scripts/Levels/Objects/SpikeTeleport.cs
+++ Assets/Scripts/Levels/Objects/SpikeTeleport.cs
@@ -6,6 +6,15 @@ public class SpikeTeleport : MonoBehaviour
 {
 
     [SerializeField] Transform teleportLocation;
+	//these two are used to get the script for the player's sound effects
+	private GameObject sfx_holder;
+	private PlayerSFX sfx;
+	
+	void Awake (){
+		//Get the objects needed for the sound effects.
+		sfx_holder = GameObject.Find("PlayerGraphics");
+		sfx = sfx_holder.GetComponent<PlayerSFX>();
+	}
 
 
     void OnTriggerEnter2D (Collider2D other)
@@ -13,6 +22,8 @@ public class SpikeTeleport : MonoBehaviour
          if (other.gameObject.tag == "Player")
          {
             other.transform.position = teleportLocation.position;
+			//play the player death sound using PlayerSFX
+			sfx.Died();
          }
      }
 }
