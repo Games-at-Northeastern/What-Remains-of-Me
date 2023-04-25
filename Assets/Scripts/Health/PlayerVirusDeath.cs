3a2389cc88577d60fe7b13e56d233bd314c542df
junye0608 <zhang.junye@northeastern.edu>
2023-04-25 14:30:16 -0400

    Virus kill script

---
+++ Assets/Scripts/Health/PlayerVirusDeath.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVirusDeath : MonoBehaviour
+{
+    [SerializeField] Transform teleportLocation;
+    //these two are used to get the script for the player's sound effects
+    private GameObject sfx_holder;
+    private PlayerSFX sfx;
+
+    void Awake (){
+        //Get the objects needed for the sound effects.
+        sfx_holder = GameObject.Find("PlayerGraphics");
+        sfx = sfx_holder.GetComponent<PlayerSFX>();
+    }
+
+    // Update is called once per frame
+    void Update(PlayerInfo playerInfo, Collider2D other)
+    {
+        if (playerInfo.virus == 100)
+        {
+            other.transform.position = teleportLocation.position;
+            InkDialogueVariables.deathCount++;
+            //play the player death sound using PlayerSFX
+            sfx.Died();
+        }
+    }
+}
