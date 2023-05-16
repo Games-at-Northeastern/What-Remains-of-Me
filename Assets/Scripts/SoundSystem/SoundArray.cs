ae77406657cd4c281d8478665b91ad78a4003295
Austin Szema <aszema13@gmail.com>
2023-05-16 18:55:15 -0400

    Updated SoundArray.cs to use Array.Find
    Instead of using a for loop to check if there is a Sound in the array sounds with a soundName that matches the given string, we are now using Array.Find

--- Assets/Scripts/SoundSystem/SoundArray.cs
+++ Assets/Scripts/SoundSystem/SoundArray.cs
@@ -6,19 +6,23 @@ public class SoundArray : ScriptableObject
 {
     public Sound[] sounds;
 
-    //Looks through the SoundArray sounds for the Sound
-    //in the array that has the same soundName as the given string.
-    //Plays that sound if it is in the array, throws an error otherwise
+    private AudioClip clipToReturn;
+
+    // Finds the Sound ScriptableObject in the array sounds that has the same soundName as the given string.
+    // If it finds a Sound in the array sounds that has the same soundName as the given string, it returns the clip of the Sound.
+    // Throws an error if it doesn't find a Sound in the array sounds that has the same soundName as the given string.
     public AudioClip GetSound(string soundname)
     {
-        foreach (Sound s in sounds)
+        clipToReturn = Array.Find<Sound>(sounds, sound => sound.soundName == soundname).clip;
+
+        if(clipToReturn != null)
         {
-            if (s.soundName == soundname)
-            {
-                return s.clip;
-            }
+            return clipToReturn;
         }
-        throw new Exception("No Sound with the name " + soundname + " found in the SoundArray");
+        else
+        {
+            throw new Exception("No Sound with the name " + soundname + " found in the array sounds of SoundArray");
+        }
     }
 
 }
