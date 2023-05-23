1c3006808773e9a9d1c647bdbd76a4fc444a18e9
BleuSky03 <taksin@photomann.com>
2023-05-23 10:03:25 -0400

    Temporary outlet demo

--- Assets/Visuals/UI/OutletFillMeter/OutletMeter.cs
+++ Assets/Visuals/UI/OutletFillMeter/OutletMeter.cs
@@ -6,23 +6,22 @@ using UnityEngine;
 
 public class OutletMeter : MonoBehaviour
 {
-    [field: SerializeField]
-    [field: Range(0, 10)]
-    public float Limiter { get; private set; }
+    
+    [Range(0, 9)]
+    public int Limiter;
 
-    [field: SerializeField]
-    [field: Range(0, 17)]
-    public float Virus { get; private set; }
+    
+    [Range(0, 16)]
+    public int Virus;
 
-    [field: SerializeField]
-    [field: Range(0, 17)]
-    public float Charge { get; private set; }
+    [Range(0, 16)]
+    public int Charge;
 
-    public Animation healthBarAnimation;
+    /*public Animation healthBarAnimation;
     public string animationClipName = "HealthBarAnimation";
-    public int targetFrame = 5;
+    public int targetFrame = 5;*/
 
-    private void Start()
+    /*private void Start()
     {
         ChangeAnimationFrame();
     }
@@ -39,7 +38,7 @@ public class OutletMeter : MonoBehaviour
         healthBarAnimation[animationClipName].time = targetTime;
         healthBarAnimation.Sample();  // Force the animation to the desired frame
         healthBarAnimation.Stop();  // Stop the animation to freeze it at the desired frame
-    }
+    }*/
 
     /*[SerializeField] private SpriteSheetData scriptable;
 
@@ -47,40 +46,42 @@ public class OutletMeter : MonoBehaviour
     [SerializeField] private string virusPath;
     [SerializeField] private string chargePath;
 
-    *//*[SerializeField] private Sprite limiterPath;
+    [SerializeField] private Sprite limiterPath;
     [SerializeField] private Sprite virusPath;
-    [SerializeField] private Sprite chargePath;*//*
+    [SerializeField] private Sprite chargePath;*/
 
-    public Sprite[] limiterSprites;
-    private Sprite[] virusSprites;
-    private Sprite[] chargeSprites;
+    [SerializeField] private Sprite[] limiterSprites;
+    [SerializeField] private Sprite[] virusSprites;
+    [SerializeField] private Sprite[] chargeSprites;
 
-    [SerializeField] private GameObject limiterMeter;
-    [SerializeField] private GameObject virusMeter;
-    [SerializeField] private GameObject chargeMeter;
+    [SerializeField] private SpriteRenderer limiterMeter;
+    [SerializeField] private SpriteRenderer virusMeter;
+    [SerializeField] private SpriteRenderer chargeMeter;
 
     private void Awake()
     {
         //limiterSprites = Resources.LoadAll<Sprite>(AssetDatabase.GetAssetPath(limiterMeter.GetComponent<SpriteRenderer>().sprite.GetInstanceID()).Replace(".png", "").Replace(limiterPath, ""));
         //virusSprites = Resources.LoadAll<Sprite>(AssetDatabase.GetAssetPath(virusMeter.GetComponent<SpriteRenderer>().sprite.GetInstanceID()).Replace(".png", "").Replace(virusPath, ""));
         //chargeSprites = Resources.LoadAll<Sprite>(AssetDatabase.GetAssetPath(chargeMeter.GetComponent<SpriteRenderer>().sprite.GetInstanceID()).Replace(".png", "").Replace(chargePath, ""));
-        Sprite[] sprites = new Sprite[scriptable.sprites.Length];
+        /*Sprite[] sprites = new Sprite[scriptable.sprites.Length];
         for (int i = 0; i < scriptable.sprites.Length; i++)
         {
             sprites[i] = (Sprite)scriptable.sprites[i];
         }
-        limiterMeter.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        limiterMeter.GetComponent<SpriteRenderer>().sprite = sprites[0];*/
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }*/
+        limiterMeter.sprite = limiterSprites[Limiter];
+        virusMeter.sprite = virusSprites[Virus];
+        chargeMeter.sprite = chargeSprites[Charge];
+    }
 }
