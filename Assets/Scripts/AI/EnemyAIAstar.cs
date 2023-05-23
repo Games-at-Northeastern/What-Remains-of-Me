8bf922f0ab2191b81ae997fb6131ab9d8fa06278
rishavsarma5 <rishavsarma5@gmail.com>
2023-05-23 02:13:20 -0400

    enemy now moves and jumps with player

--- Assets/Scripts/AI/EnemyAIAstar.cs
+++ Assets/Scripts/AI/EnemyAIAstar.cs
@@ -1,11 +1,13 @@
 using Pathfinding;
 using UnityEngine;
 
-
+/// <summary>
+/// Class that deals with enemy AI path finding towards a target (player).
+/// Uses the A* Pathfinding library package included in the assets folder.
+/// Code adapted from this YT video: https://www.youtube.com/watch?v=sWqRfygpl4I&ab_channel=Etredal
+/// </summary>
 public class EnemyAIAstar : MonoBehaviour
 {
-    // ADAPTED FROM THIS YT VID: https://www.youtube.com/watch?v=sWqRfygpl4I&ab_channel=Etredal
-
     [Header("Pathfinding")]
     // object enemey is following (should be player)
     public Transform target;
@@ -17,52 +19,58 @@ public class EnemyAIAstar : MonoBehaviour
     [Header("Physics")]
     // how fast enemy moves
     public float walkSpeed = 50f;
-
+    // how far away the enemy needs to be to start moving to next waypoint
     public float nextWaypointDistance = 3f;
+    // how vertical next node needs to be for enemy to jump
     public float minimimumHeightForJump = 5f;
+    // how powerful jump is
     public float jumpForce = 40f;
+    // for the collider with nodes offset 
     public float jumpColliderOffset = 0.1f;
 
     // add behavior modifiers as necessary for enemy ai navigation
     [Header("Custom Enemy Behavior Modifiers")]
+    // toggle enable enemy to follow (should be true for script to work)
     public bool followEnabled = true;
+    // toggle enable enemy to be able to jump
     public bool jumpEnabled = true;
+    // toggle enable enemy to look at target
     public bool lookAtEnabled = true;
 
     // private variables
+    // use variable provided by A* Pathfinding library package
     private Path path;
+    // variable to mark current waypoint
     private int currentWaypoint = 0;
-    bool isGrounded = false;
+
+    // what layer all ground objects enemy detects should be on
+    [SerializeField] private LayerMask groundLayer;
     bool reachedEndOfPath = false;
+    //private Animator anim;
     Seeker seeker;
     Rigidbody2D rb;
+    Collider2D col;
 
+    /// <summary>
+    /// Initializes seeker script among other private variables.
+    /// Starts process of calling UpdatePath towards target.
+    /// </summary>
+    /// <returns></returns>
     private void Start()
     {
-        // get seeker script and rigidbody from enemy object
+        // get seeker script, rigidbody, and collider from enemy object
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
 
+        // repeatedly calls UpdatePath func based on update variable
         InvokeRepeating("UpdatePath", 0, updatePathPerSecondSpeed);
     }
 
-    private void UpdatePath()
-    {
-        if (followEnabled && TargetInDistance() && seeker.IsDone())
-        {
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
-        }
-    }
-
-    private void OnPathComplete(Path p)
-    {
-        if (!p.error)
-        {
-            path = p;
-            currentWaypoint = 0;
-        }
-    }
-
+    /// <summary>
+    /// Sets the enemy to move toward player if player within activate distance and follow is enabled.
+    /// </summary>
+    /// <returns></returns>
     private void FixedUpdate()
     {
         if (TargetInDistance() && followEnabled)
@@ -71,6 +79,31 @@ public class EnemyAIAstar : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Updates the pathfinding algo to find next waypoint towards target (player) if target is in distance.
+    /// If target is reached, OnPathComplete is called.
+    /// </summary>
+    /// <returns></returns>
+    private void UpdatePath()
+    {
+        if (followEnabled && TargetInDistance() && seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    /// <summary>
+    /// Sets path to given path and resets current waypoint. This means enemy has reached target.
+    /// </summary>
+    /// <returns></returns>
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
 
     /// <summary>
     /// Path is done if it has reached the end of path, or it's nonexistant.
@@ -90,6 +123,10 @@ public class EnemyAIAstar : MonoBehaviour
         return false;
     }
 
+    /// <summary>
+    /// Handles updating enemy movement toward target (player).
+    /// </summary>
+    /// <returns></returns>
     private void MoveTowardsPlayer() {
 
         if (IsPathDone())
@@ -98,11 +135,12 @@ public class EnemyAIAstar : MonoBehaviour
         }
 
 
-        // checking if enemy is grounded 
-        isGrounded = Physics2D.Raycast(transform.position, Vector3.up, GetComponent<Collider2D>().bounds.extents.y + jumpColliderOffset);
+        // checking if enemy is grounded
+        RaycastHit2D isGrounded = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
 
+        // direction calculation
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 moveForce = direction * walkSpeed * Time.deltaTime;
+        //Vector2 moveForce = direction * walkSpeed * Time.deltaTime;
 
         // Jump
         if (jumpEnabled && isGrounded)
@@ -114,7 +152,15 @@ public class EnemyAIAstar : MonoBehaviour
         }
 
         // Movement
-        rb.AddForce(moveForce);
+        rb.AddForce(Vector2.right * direction, ForceMode2D.Impulse);
+
+        if (rb.velocity.x > walkSpeed)
+        {
+            rb.velocity = new Vector2(walkSpeed, rb.velocity.y);
+        } else if (rb.velocity.x < -walkSpeed)
+        {
+            rb.velocity = new Vector2(-walkSpeed, rb.velocity.y);
+        }
 
         // move to next waypoint
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
@@ -135,11 +181,12 @@ public class EnemyAIAstar : MonoBehaviour
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
         }
-
-             
-       
     }
 
+    /// <summary>
+    /// Checks whether target (player) is within activateDistance of enemy. Will start to follow if true.
+    /// </summary>
+    /// <returns>boolean</returns>
     private bool TargetInDistance()
     {
         return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
