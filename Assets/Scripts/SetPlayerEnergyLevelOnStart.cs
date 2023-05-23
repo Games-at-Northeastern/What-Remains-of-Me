e0d4949bee8a7a2b491e7a9c819139befea36211
Bosspug <eugeniojlopezt@gmail.com>
2023-05-23 17:10:08 -0400

    Level Ideas and slight movement touch
    New level ideas and made Atlas slightly more mobile to make him a tad bit more self sufficient and not so reliant on outlets

--- Assets/Scripts/SetPlayerEnergyLevelOnStart.cs
+++ Assets/Scripts/SetPlayerEnergyLevelOnStart.cs
@@ -12,7 +12,7 @@ public class SetPlayerEnergyLevelOnStart : MonoBehaviour
     [Header("How much energy the player should start with between 0 and 1")]
     [SerializeField] private float PlayerEnergyStartingValue = 1f;
     [Header("How much virus the player should start with between 0 and 1")]
-    [SerializeField] private float PlayerVirusStartingValue = 1f;
+    [SerializeField] private float PlayerVirusStartingValue = 0f;
 
 
     void Start()
