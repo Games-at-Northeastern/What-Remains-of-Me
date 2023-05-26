b8baae7f46ee2496a9f14bf6e54e923c104eeb41
Austin Szema <aszema13@gmail.com>
2023-05-26 15:39:30 -0400

    Updated Crushing Gears Prefab to have a death animation and updated rocky ground rule tile to use 32x32
    - Updated Crushing Gears Prefab to have a death animation using the DeathParticles Prefab
    
    - Updated rocky ground rule tile to use the new sprite that is 32x32 pixels

--- Assets/Scripts/Levels/Objects/SpikeTeleport.cs
+++ Assets/Scripts/Levels/Objects/SpikeTeleport.cs
@@ -10,6 +10,12 @@ public class SpikeTeleport : MonoBehaviour
     private GameObject sfx_holder;
     private PlayerSFX sfx;
 
+
+    [SerializeField] private ParticleSystem deathParticles;
+
+    private GameObject objectToTeleport;
+
+
     private void Awake()
     {
         //Get the objects needed for the sound effects.
@@ -22,18 +28,45 @@ public class SpikeTeleport : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            // TODO: Remove this once CheckpointManagers have been placed in every scene. Until then,
-            // keep this check to make sure no current scenes break
-            if (FindObjectOfType<CheckpointManager>() == null)
-            {
-                other.gameObject.transform.position = teleportLocation.position;
-            }
 
-            LevelManager.Instance.PlayerReset();
-            InkDialogueVariables.deathCount++;
 
-            //play the player death sound using PlayerSFX
-            sfx.Died();
+            objectToTeleport = other.gameObject;
+            deathParticles.gameObject.transform.position = objectToTeleport.transform.position;
+            deathParticles.Clear();
+            deathParticles.Play();
+            objectToTeleport.SetActive(false);
+
+            Invoke("TeleportPlayer", deathParticles.main.duration);
+
         }
     }
+
+
+
+
+
+
+
+    private void TeleportPlayer()
+    {
+
+
+        // TODO: Remove this once CheckpointManagers have been placed in every scene. Until then,
+        // keep this check to make sure no current scenes break
+        if (FindObjectOfType<CheckpointManager>() == null)
+        {
+
+            objectToTeleport.transform.position = teleportLocation.position;
+            objectToTeleport.SetActive(true);
+
+        }
+
+        LevelManager.Instance.PlayerReset();
+        InkDialogueVariables.deathCount++;
+
+        //play the player death sound using PlayerSFX
+        sfx.Died();
+    }
 }
+
+
