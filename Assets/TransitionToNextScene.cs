eaf6172e2c6f78d6ad4745cba64229eac82e37f4
Austin Szema <aszema13@gmail.com>
2023-06-08 16:05:06 -0400

    Updated TransitionToNextScene script to use CompareTag instead of Tag
    Updated the TransitionToNextScene script to use CompareTag instead of Tag when checking to see if the player is within the trigger

--- Assets/TransitionToNextScene.cs
+++ Assets/TransitionToNextScene.cs
@@ -10,7 +10,7 @@ public class TransitionToNextScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene(sceneID);
         }
