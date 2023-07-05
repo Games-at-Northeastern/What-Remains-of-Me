4979ec9aed51143499a1e725384f45748a19837a
Austin Szema <aszema13@gmail.com>
2023-07-05 18:52:07 -0400

    Added SocialMediaLinks Scene
    Added a scene that will lead the player to all of our community management created social media platforms by clicking the logos

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OpenLink : MonoBehaviour
+{
+    private enum socialMedia { Discord, Itch, Website, Twitter, Instagram };
+
+    [SerializeField] private socialMedia app;
+
+    private string url; 
+
+    private void OnMouseDown()
+    {
+        OpenURL();
+    }
+
+    public void OpenURL()
+    {
+        switch (app)
+        {
+            case socialMedia.Discord:
+                url = "https://discord.gg/fBBBanPCHr";
+                break;
+            case socialMedia.Itch:
+                url = "https://whatremainsofme.itch.io";
+                break;
+            case socialMedia.Website:
+                url = "https://games.northeastern.edu/studio/";
+                break;
+            case socialMedia.Twitter:
+                url = "https://twitter.com/remains_game";
+                break;
+            case socialMedia.Instagram:
+                url = "https://instagram.com/whatremainsofme_game/";
+                break;
+        }
+        Debug.Log(url);
+        Application.OpenURL(url);
+    }
+
+}
