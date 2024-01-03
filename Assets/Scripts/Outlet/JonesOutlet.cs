edbf82ad5f31eac959048b929a38d5e2da533826
Christopher A. Barney <c.barney@northeastern.edu>
2024-01-03 11:59:16 -0500

    Updated JonesOtlet.cs to open animated force doors
    Updated the script to also disable the animators. Now controls three doors correctly. Need to make the exit door a different system based on the voice controler.

--- Assets/Scripts/Outlet/JonesOutlet.cs
+++ Assets/Scripts/Outlet/JonesOutlet.cs
@@ -8,14 +8,17 @@ public class JonesOutlet : AControllable
     [SerializeField] private SpriteRenderer door;
     [SerializeField] private Sprite openDoorSprite;
     [SerializeField] private Collider2D doorCollider;
+    [SerializeField] private Animator doorAnimator;
 
     [SerializeField] private SpriteRenderer door2;
     [SerializeField] private Sprite openDoorSprite2;
     [SerializeField] private Collider2D doorCollider2;
+    [SerializeField] private Animator doorAnimator2;
     
     [SerializeField] private SpriteRenderer door3;
     [SerializeField] private Sprite openDoorSprite3;
     [SerializeField] private Collider2D doorCollider3;
+    [SerializeField] private Animator doorAnimator3;
 
     [SerializeField] private Slider slider;
 
@@ -28,12 +31,15 @@ public class JonesOutlet : AControllable
         {
             explosionParticles.Play();
             door.sprite = openDoorSprite;
+            doorAnimator.enabled = false;
             doorCollider.enabled = false;
 
-	    door2.sprite = openDoorSprite2;
+	        door2.sprite = openDoorSprite2;
+            doorAnimator2.enabled = false;
             doorCollider2.enabled = false;
 
-	    door3.sprite = openDoorSprite3;
+	        door3.sprite = openDoorSprite3;
+            doorAnimator3.enabled = false;
             doorCollider3.enabled = false;
         }
     }
