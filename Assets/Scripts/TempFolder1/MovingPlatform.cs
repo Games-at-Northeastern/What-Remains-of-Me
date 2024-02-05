9f915ceac993ec82ae74d5f53ceca46032258429
Giacomo Mantovanelli <104456506+giacomoManto@users.noreply.github.com>
2024-02-04 21:58:36 -0500

    FixBuggyJump on platforms moving down

--- Assets/Scripts/TempFolder1/MovingPlatform.cs
+++ Assets/Scripts/TempFolder1/MovingPlatform.cs
@@ -6,6 +6,8 @@ public class MovingPlatform : MovingElement
 {
     protected virtual bool IsOnTop(Vector2 normal) => Vector2.Dot(transform.up, normal) < -0.5f;
 
+    private Vector2 velocity = new Vector2(0 ,0);
+
     PlayerController2D player;
     protected virtual void OnCollisionEnter2D(Collision2D col)
     {
@@ -24,16 +26,12 @@ public class MovingPlatform : MovingElement
             player = null;
         }
     }
+
+    public Vector2 GetVelocity() => velocity;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (player != null)
-        {
-            player.ExternalVelocity = MovePlatform();
-        }
-        else
-        {
-            MovePlatform();
-        }
+        velocity = MovePlatform();
     }
 }
