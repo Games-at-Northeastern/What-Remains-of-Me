f60b7725e32f7d640bff8f3383c2f18d0abae82c
Giacomo Mantovanelli <104456506+giacomoManto@users.noreply.github.com>
2024-04-15 17:41:28 -0400

    Fixed virus turrets displaying visual when they shouldn't be

--- Assets/Scripts/Levels/Objects/VirusTurret.cs
+++ Assets/Scripts/Levels/Objects/VirusTurret.cs
@@ -37,7 +37,7 @@ public class VirusTurret : MonoBehaviour
 
     public AudioSource audioSource;
 
-    private bool activateVisual = true;
+    private bool activateVisual = false;
 
     private void Start()
     {
