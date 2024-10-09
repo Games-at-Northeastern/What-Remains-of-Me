3f52c235e6fec2fd854d849477b3d3ea4ef997b6
Vi <haggertyv@icloud.com>
2024-10-09 17:09:43 -0400

    Organized the Jump script

--- Assets/Scripts/CharacterController/New Controller/IMove/Jump.cs
+++ Assets/Scripts/CharacterController/New Controller/IMove/Jump.cs
@@ -7,13 +7,18 @@ namespace CharacterController
     /// </summary>
     public class Jump : IMove
     {
-        private readonly float initialVelocity;
-        private float risingGravity;
-        private JumpType jumpType;
-        private CharacterController2D character;
-        private readonly float timeToReachApex;
-        private float timePassed;
-        private bool jumpCanceled;
+        private readonly float initialVelocity; //The velocity at the start of the jump
+        private float risingGravity; //The external force of gravity on the player
+        private JumpType jumpType; //Determines how the initial velocity is added to the player (add vs. set)
+        private CharacterController2D character; //The character that is jumping
+        private readonly float timeToReachApex; //The time until the player reaches the peak of the jump
+        private float timePassed; //The time that has passed since the player started jumping
+        private bool jumpCanceled; //Has the jump been cancelled?
+
+        /// <summary>
+        /// Constructs a new jump, calculating the initial velocity based off of gravity + height,
+        /// and timeToReachApex using that initial velocity. 
+        /// </summary>
         public Jump(float risingGravity, float jumpHeight, JumpType jumpType, CharacterController2D character)
         {
             this.risingGravity = risingGravity;
@@ -22,21 +27,29 @@ namespace CharacterController
             this.jumpType = jumpType;
             this.timeToReachApex = -initialVelocity / risingGravity;
         }
+
+        /// <summary>
+        /// Cancels this jump. 
+        /// </summary>
         public void CancelMove() =>
             // empty for now but i think it can be useful in regards to jumpBuffers
             // though i may want that in the character controller itself
             jumpCanceled = true;
 
+        /// <summary>
+        /// Returns the animation state for this action. 
+        /// </summary>
         public AnimationType GetAnimationState() => AnimationType.JUMP_RISING;
+
         /// <summary>
         /// is true after character hits apex of the jump
         /// </summary>
         /// <returns></returns>
         public bool IsMoveComplete() => timePassed >= timeToReachApex || jumpCanceled;
+
         /// <summary>
         /// Changes the character y velocity to what it should be at the beginning of this jump
         /// </summary>
-
         public void StartMove()
         {
             timePassed = 0;
@@ -51,6 +64,7 @@ namespace CharacterController
                     break;
             }
         }
+
         /// <summary>
         /// decreases gravity while the player holds down space;
         /// </summary>
@@ -60,6 +74,7 @@ namespace CharacterController
             character.Speed = new Vector2(character.Speed.x, character.Speed.y - (risingGravity * Time.fixedDeltaTime));
         }
     }
+    
     /// <summary>
     /// There are 2 jump types
     /// AddSpeed: will add some Velocity Value v to your current y Speed
