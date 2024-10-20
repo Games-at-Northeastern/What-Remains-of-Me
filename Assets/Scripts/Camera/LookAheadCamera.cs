a79e752c7d5de69f9c9e20a993dae2dc48706ce1
BewThanin <kongkiatsophon.t@northeastern.edu>
2024-10-20 18:23:15 -0400

    Create LookAheadCamera.cs

---
+++ Assets/Scripts/Camera/LookAheadCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LookAheadCamera : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCam;
+    private CinemachineFramingTransposer transposer;
+
+    private float maxLookAheadDistance = 3.5f; //max distance the camera can look ahead.
+    private float lookAheadSpeed = 2f; //speed of camera movement.
+    private Camera thisCamera;
+
+    void Start()
+    {
+        //get the framing transposer component from the virtual camera
+        transposer = virtualCam.GetCinemachineComponent<CinemachineFramingTransposer>();
+        thisCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        //get the mouse position in the world
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        //get the center of the screen in world coordinates
+        Vector3 screenCenter = thisCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        //calculate the offset based on how far the mouse is from the center of the screen
+        Vector3 offset = mousePos - screenCenter;
+
+        //clamp the offset to the maximum lookahead distance
+        offset = Vector3.ClampMagnitude(offset, maxLookAheadDistance);
+
+        //smoothly adjust the camera offset
+        transposer.m_TrackedObjectOffset = Vector3.Lerp(transposer.m_TrackedObjectOffset, offset, lookAheadSpeed * Time.deltaTime);
+    }
+}
+x
