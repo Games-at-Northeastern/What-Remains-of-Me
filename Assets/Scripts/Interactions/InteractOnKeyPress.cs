22c124674f8675eedcfa0b1ca6d4652a7e634650
Aidsmau <moreau.ai@northeastern.edu>
2024-11-05 16:45:04 -0500

    Created UpdateOnKeyPress script

--- Assets/Scripts/Interactions/InteractOnKeyPress.cs
+++ Assets/Scripts/Interactions/InteractOnKeyPress.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class InteractOnKeyPress : GenericInteractiveScript
 {
+<<<<<<< Updated upstream
     protected override void AttemptInteract()
     {
         playerInRange = false;
+=======
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+>>>>>>> Stashed changes
+    }
+
+    protected override void AttemptInteract()
+    {
+        if (base._cs.Player.Dialogue.WasPressedThisFrame())
+        {
+            Interact();
+        }
+
+    }
+
 }
