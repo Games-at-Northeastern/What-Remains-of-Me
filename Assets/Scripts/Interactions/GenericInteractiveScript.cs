896fcaa82e60e0e49e07d5f5ab66615b22b94325
Playstarstudio <charles@playstarstudio.com>
2024-11-13 18:36:02 -0500

    Update to interactive scripts and boss2 room

--- Assets/Scripts/Interactions/GenericInteractiveScript.cs
+++ Assets/Scripts/Interactions/GenericInteractiveScript.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+
 public abstract class GenericInteractiveScript : MonoBehaviour
 {
-
+    public List<string> tagsToInteractWith = new List<string> { "Player", "Interactable" };
     private Interaction interaction;
-    protected bool playerInRange = false;
+    protected bool objectInRange = false;
+
 
     protected ControlSchemes _cs;
-    private BoxCollider2D _collider;
-    // Start is called before the first frame update
+
+
     void Start()
     {
         Init();
@@ -20,43 +23,52 @@ public abstract class GenericInteractiveScript : MonoBehaviour
     protected virtual void Init()
     {
         Debug.Log("starting to interact");
+
+
+
+
         interaction = GetComponent<Interaction>();
-        _collider = GetComponent<BoxCollider2D>();
         _cs = new ControlSchemes();
         _cs.Enable();
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        if (playerInRange)
+        if (objectInRange)
         {
             AttemptInteract();
         }
     }
 
+
     protected abstract void AttemptInteract();
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (tagsToInteractWith.Contains(other.gameObject.tag))
         {
-            playerInRange = true;
+            objectInRange = true;
         }
     }
 
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (tagsToInteractWith.Contains(other.gameObject.tag))
         {
-            playerInRange = false;
+            objectInRange = false;
         }
     }
+
 
     protected void Interact()
     {
         interaction.Execute();
-        Debug.Log("We are interacting rn.");
     }
 
+
 }
+
