5e27c6373bbf4aba6d776c60fd5a937256ab2fc3
scrypticoa <scrypticoa@gmail.com>
2024-11-13 20:12:53 -0500

    Convert Rule Tiles to Connected Tiles

--- Assets/tilebasestorage.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-public class tilebasestorage : MonoBehaviour
-{
-    [SerializeField] private TileBase tb;
-}
