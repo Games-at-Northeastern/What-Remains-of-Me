fc9561ceab6a3fccf5a3fa412c54bd004cec5e36
Jaylen Zeng <jaylenzeng@gmail.com>
2024-11-20 16:16:51 -0500

    Updated JonesOutlet fields for only 1 door

--- Assets/Scripts/Outlet/JonesOutlet.cs
+++ Assets/Scripts/Outlet/JonesOutlet.cs
@@ -11,11 +11,6 @@ public class JonesOutlet : AControllable
     [SerializeField] private Collider2D doorCollider2;
     [SerializeField] private Animator doorAnimator2;
 
-    [SerializeField] private SpriteRenderer door3;
-    [SerializeField] private Sprite openDoorSprite3;
-    [SerializeField] private Collider2D doorCollider3;
-    [SerializeField] private Animator doorAnimator3;
-
     [SerializeField] private Slider slider;
 
     [SerializeField] private ParticleSystem explosionParticles;
@@ -28,10 +23,6 @@ public class JonesOutlet : AControllable
             door2.sprite = openDoorSprite2;
             doorAnimator2.enabled = false;
             doorCollider2.enabled = false;
-
-            door3.sprite = openDoorSprite3;
-            doorAnimator3.enabled = false;
-            doorCollider3.enabled = false;
         }
     }
 }
