52c440e8be855145f785ebb0bd077611fb195032
Aidan Moreau <moreau.ai@northeastern.edu>
2025-02-03 11:26:19 -0500

    Created a NullControllable Object

---
+++ Assets/Scripts/Wire/IControllable/NullControllable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NullControllable : AControllable
+{
+
+    void Start()
+    {
+        cleanEnergy = 0;
+        maxCharge = 0;
+        virus = 0;
+
+    }
+}
