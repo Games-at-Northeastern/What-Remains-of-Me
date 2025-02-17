994154e6362070f599a357ee4b85669541a33123
GoForGoldner <goldner.t@northeastern.edu>
2025-02-16 19:59:54 -0500

    Create a ending panning sequence using Cinemachine for 1.1.1
    Jira Task: https://games-at-northeastern-studio.atlassian.net/browse/GS-5461
    
    QA script: https://docs.google.com/spreadsheets/d/1q2MFK175tYe3nX20L3KgeOnQg1xmIW55vL91AH7rB3Y/edit?gid=0#gid=0
    
    Created the ending cutscene sequence for Atlas before he jumps of the platform in 1.1.1!

---
+++ Assets/Scripts/Camera/SimpleTransitionScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+// Make sure the GameObject has the necessary components!
+[RequireComponent(typeof(Cinemachine.CinemachineVirtualCamera), typeof(Collider2D))]
+public class SimpleTransitionScript : MonoBehaviour {
+    private Cinemachine.CinemachineVirtualCamera cam;
+
+    // Gets the camera from the current GameObject
+    private void Awake() => cam = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player")) /* If the player entered the camera */
+        { 
+            cam.Priority = 100;  // Make the priority of the camera very high
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player")) /* If the player left the camera */
+        {
+            cam.Priority = -100; // Make the priority of the camera very low
+        }
+    }
+}
