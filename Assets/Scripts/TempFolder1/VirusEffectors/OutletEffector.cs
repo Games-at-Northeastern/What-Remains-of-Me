b413fa62db4d08dfc7947f5c75858187ba47918c
karysfontana <karysfontana@gmail.com>
2025-03-11 18:17:26 -0400

    GS-5462
    Added OutletEffector script (name can be changed since it's a general version of the ChaosEffector script)

---
+++ Assets/Scripts/TempFolder1/VirusEffectors/OutletEffector.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+/// <summary>
+/// When the virus affecting this element gets large enough, overrides the path and loop type (works for outlets).
+/// </summary>
+public class OutletEffector : AMovingElementVirusEffector
+{
+    [SerializedField] private Transform[] _chargedPath; 
+
+    [SerializedField] private LoopType _overrideLoopType; 
+
+    private bool _shouldRevertPath = true; 
+
+    private void Awake()
+    {
+        if (_doVirusEffectAt >= 0.5f) {
+            Debug.LogWarning("DoVirusEffectAt var has a really high value. It might not trigger."); 
+        }
+    }
+
+    protected override void AffectMovingOutlet(MovingElement element) => 
+        element.SetTrack(_chargedPath, _overrideLoopType, _shouldRevertPath); 
+    
+    /// <summary>
+    /// Checks if the effect should be applied based on 
+    /// <summary>
+    protetected override bool ShouldDoEffect(float newChargePercentage)
+    {
+        if ((_shouldRevertPath && newChargePercentage >= _doVirusEffectAt) || 
+            (!_shouldRevertPath && newChargePercentage < _doVirusEffectAt))
+        {
+            _shouldRevertPath = !_shouldRevertPath;
+            return true;
+        }
+
+        return false; 
+    }
+}
