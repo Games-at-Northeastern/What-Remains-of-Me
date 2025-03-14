f36fb439c8e5aba2a5be0921d314ddff2dab2338
Aidan Moreau <moreau.ai@northeastern.edu>
2025-03-14 19:44:34 -0400

    Added ambient trigger

---
+++ Assets/OrbServerAmbientDialogue.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class OrbServerAmbientDialogue : InkDialogueAmbientTrigger
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartZeroNodeTimer() {
+        
+    }
+}
