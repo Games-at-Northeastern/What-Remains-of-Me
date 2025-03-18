56a2930db8026b8fec44660f0346212b501ce7aa
ryantheking603 <91330332+ryantheking603@users.noreply.github.com>
2025-03-18 12:45:00 -0400

    Commented the ShadowCasterApplicator script

--- Assets/Scripts/Visuals/ShadowCasterApplicator.cs
+++ Assets/Scripts/Visuals/ShadowCasterApplicator.cs
@@ -4,6 +4,8 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine.Rendering.Universal;
 
+// the #if ensures the script is not compiled when making a build, because it is only meant to work in the Editor.
+// Once the casters are applied, no need to compile this script with the rest.
 #if UNITY_EDITOR
 
 public class ShadowCasterApplicator : MonoBehaviour
@@ -11,7 +13,6 @@ public class ShadowCasterApplicator : MonoBehaviour
     [SerializeField] private bool selfShadows = true;
     private CompositeCollider2D tilemapCollider;
 
-    //static readonly FieldInfo meshField = typeof(ShadowCaster2D).GetField("m_Mesh", BindingFlags.NonPublic | BindingFlags.Instance);
 	static readonly FieldInfo shapePathField = typeof(ShadowCaster2D).GetField("m_ShapePath", BindingFlags.NonPublic | BindingFlags.Instance);
 	static readonly FieldInfo shapePathHashField = typeof(ShadowCaster2D).GetField("m_ShapePathHash", BindingFlags.NonPublic | BindingFlags.Instance);
 	static readonly MethodInfo generateShadowMeshMethod = typeof(ShadowCaster2D)
@@ -21,10 +22,10 @@ public class ShadowCasterApplicator : MonoBehaviour
 
     public void Create()
     {
-        //Debug.Log(meshField);
         DestroyOldShadowCasters();
         tilemapCollider = GetComponent<CompositeCollider2D>();
 
+        // for all paths in the collider, which I think of as different seperated chunks, it creates a new GameObject, makes it a child of this tilemap, and adds a ShadowCaster2D component to it
         for (int i = 0; i < tilemapCollider.pathCount; i++)
         {
             Vector2[] pathVertices = new Vector2[tilemapCollider.GetPathPointCount(i)];
@@ -34,6 +35,7 @@ public class ShadowCasterApplicator : MonoBehaviour
             ShadowCaster2D shadowCasterComponent = shadowCaster.AddComponent<ShadowCaster2D>();
             shadowCasterComponent.selfShadows = this.selfShadows;
 
+            // shapes the new shadow caster to fit this piece of tilemap geometry
             Vector3[] testPath = new Vector3[pathVertices.Length];
             for (int j = 0; j < pathVertices.Length; j++)
             {
@@ -42,9 +44,6 @@ public class ShadowCasterApplicator : MonoBehaviour
 
             shapePathField.SetValue(shadowCasterComponent, testPath);
             shapePathHashField.SetValue(shadowCasterComponent, Random.Range(int.MinValue, int.MaxValue));
-            //meshField.SetValue(shadowCasterComponent, new Mesh());
-            //generateShadowMeshMethod.Invoke(shadowCasterComponent, new object[] { new Mesh(), shapePathField.GetValue(shadowCasterComponent)});
-            //meshField.GetValue(shadowCasterComponent)
         }
     }
 
@@ -58,6 +57,7 @@ public class ShadowCasterApplicator : MonoBehaviour
     }
 }
 
+// This is the GUI that appears in the Inspector, allowing for the 'Create' and 'Remove shadows' buttons.
 [CustomEditor (typeof(ShadowCasterApplicator))]
 public class ShadowCaster2DTileMapEditor : Editor
 {
