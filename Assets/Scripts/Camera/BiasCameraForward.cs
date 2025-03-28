8972f598d32e0fe39ff5a9c6fd37a79063e726e3
GoForGoldner <goldner.t@northeastern.edu>
2025-03-28 12:01:17 -0400

    GS-5869 Remove mouse effecting main camera
    Jira Task: https://games-at-northeastern-studio.atlassian.net/browse/GS-5869
    QA script: https://docs.google.com/spreadsheets/d/1QlrkF7ieAYmtCQmumY2X7R-x7SZ6fX1yF-dRkxsx9JI/edit?gid=0#gid=0
    
    Changed the camera from being affected by the mouse's position to instead the direction Atlas is facing. The mouse position script is still a possible setting that could be reenabled at a future point (or put in the settings menu eventually).

---
+++ Assets/Scripts/Camera/BiasCameraForward.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+using Cinemachine;
+using PlayerController;
+
+public class BiasCameraForward : MonoBehaviour {
+    [SerializeField] private PlayerController2D player;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private float screenBias = 0.1f;
+    [SerializeField] private float smoothingSpeed = 5f;
+
+    private CinemachineFramingTransposer transposer;
+    private float targetScreenX;
+    private float currentScreenX;
+
+    private void Start() {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        currentScreenX = 0.5f;
+        transposer.m_ScreenX = currentScreenX;
+    }
+
+    private void Update() {
+        targetScreenX = player.LeftOrRight == CharacterController.Facing.left
+            ? 0.5f - screenBias
+            : 0.5f + screenBias;
+
+        currentScreenX = Mathf.Lerp(currentScreenX, targetScreenX, smoothingSpeed * Time.deltaTime);
+        transposer.m_ScreenX = currentScreenX;
+    }
+}
