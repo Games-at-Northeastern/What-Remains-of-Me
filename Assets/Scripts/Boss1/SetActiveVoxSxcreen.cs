1fa671ea0ef5fa44c936dbeff82cd33cc7a8291d
karysfontana <karysfontana@gmail.com>
2025-04-15 11:35:10 -0400

    Animations trigger correctly
    Animations trigger correctly. Edited the setActiveScreen script to factor out a method. Will update to get animations to end correctly.

--- Assets/Scripts/Boss1/SetActiveVoxSxcreen.cs
+++ Assets/Scripts/Boss1/SetActiveVoxSxcreen.cs
@@ -11,36 +11,44 @@ public class SetActiveVoxSxcreen : MonoBehaviour
     // Triggers when dialogue triggered. 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Traverse to parent object (SmallVoxScreen#)
-        Transform root = transform.parent; 
-
-        if (root != null)
+        GameObject screen = GetVoxScreenOnTrigger(); 
+        if (screen != null)
         {
-            Transform smallVox = root.Find("SmallVox");
-            if (smallVox != null)
-            {
-                Transform voxScreen = smallVox.Find("VoxScreen");
-                if (voxScreen != null)
-                {
-                    // Sets the active screen to the current screen. 
-                    ActiveScreenManager.Instance.SetActiveScreen(gameObject);
-                    Debug.Log($"Set active VoxScreen to {voxScreen.name}");
-                }
-                else
-                {
-                    Debug.LogWarning("VoxScreen not found as a child of SmallVox");
-                }
-            }
-            else 
-            {
-                Debug.LogWarning("SmallVox not found in root object");
-            }
+            // Sets the active screen to the triggered one. 
+            ActiveScreenManager.Instance.SetActiveScreen(screen); 
+            Debug.Log($"Set active VoxScreen to {screen.name}");
         }
         else 
         {
             Debug.LogWarning("Trigger has no parent to find VoxScreen in");
         }
-        // Sets the active screen to the screen player is at. 
-        ActiveScreenManager.Instance.SetActiveScreen(gameObject);
+    }
+
+    // Factored out method to get the screen when triggered. 
+    private GameObject GetVoxScreenOnTrigger()
+    {
+        // Traverse to parent object (SmallVoxScreen#)
+        Transform root = transform.parent; 
+        if (root == null)
+        {
+            Debug.LogWarning("Trigger has no parent to find VoxScreen in");
+            return null; 
+        }
+
+        // Traverse for animator object parent (SmallVox)
+        Transform smallVox = root.Find("SmallVox"); 
+        if (smallVox == null)
+        {
+            Debug.LogWarning("SmallVox not found in root object");
+            return null; 
+        }
+
+        // Traverse for animator controller object (VoxScreen)
+        Transform voxScreen = smallVox.Find("VoxScreen"); 
+        if (voxScreen ==  null) {
+            Debug.LogWarning("VoxScreen not found as a child of SmallVox");
+            return null; 
+        }
+        return voxScreen?.gameObject; 
     }
 }
