8c38c3f28628aa33635f3bf93153a77d8b1d4300
Will Kibel <56790114+willkbl@users.noreply.github.com>
2025-04-22 14:34:28 -0400

    GS-6020 Create and implement ambient hum sound effect for lasers
    https://games-at-northeastern-studio.atlassian.net/browse/GS-6020

--- Assets/DeathLaserSound.cs
+++ Assets/DeathLaserSound.cs
@@ -5,28 +5,30 @@ using UnityEngine;
 public class DeathLaserSound : MonoBehaviour
 {
 
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        laserHum = GetComponent<AudioSource>();
+        laserVolume = laserHum.volume;
+    }
+
     public static AudioClip laserOnSound;
     public static AudioClip laserOffSound;
     private static AudioSource laserHum;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private static float laserVolume;
 
     public static void PlayLaserOnSound(Transform t)
     {
         if (laserOnSound != null)
         {
             AudioSource.PlayClipAtPoint(laserOnSound, t.position);
+        }
+        if (laserHum.volume == 0)
+        {
+            laserHum.volume = laserVolume;
         }
     }
 
@@ -35,6 +37,10 @@ public class DeathLaserSound : MonoBehaviour
         if (laserOffSound != null)
         {
             AudioSource.PlayClipAtPoint(laserOffSound, t.position);
+        }
+        if (laserHum.volume > 0)
+        {
+            laserHum.volume = 0;
         }
     }
 }
