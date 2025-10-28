5c4a0d9c0f65f7f1f2a2502dae48589d9bcf0a5c
FF-Yan <biinxuans@gmail.com>
2025-10-28 03:06:01 -0400

    Change the keyoutlet script

--- Assets/Scripts/Outlet/KeyOutlet.cs
+++ Assets/Scripts/Outlet/KeyOutlet.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using System.Collections;
 
 public class KeyGrantOutlet : Outlet
 {
     [SerializeField] private AutoOpenDoor door;
     [SerializeField] private bool grantOnce = true;
+    [SerializeField] private float delayBeforeGrant = 2f;
 
     private bool granted = false;
+    private Coroutine grantRoutine;
 
     public override void Connect()
     {
@@ -15,16 +18,33 @@ public class KeyGrantOutlet : Outlet
             return;
         if (!door)
             return;
+        if (grantRoutine != null)
+            StopCoroutine(grantRoutine);
 
-        door.SetHasKey();
-        granted = true;
-
-        Debug.Log("Player has key! ");
+        grantRoutine = StartCoroutine(GetKey());
     }
 
     public override void Disconnect()
     {
         base.Disconnect();
+        grantRoutine = null;
+    }
+
+    private IEnumerator GetKey()
+    {
+        var timer = 0f;
+
+        while (timer < delayBeforeGrant)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        door.SetHasKey();
+        granted = true;
+        Debug.Log("Player has key! ");
+
+        grantRoutine = null;
     }
 }
 
