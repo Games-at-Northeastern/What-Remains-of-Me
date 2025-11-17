fa362340a9c071a3aa7ea516b4df5e0fb040897d
FF-Yan <biinxuans@gmail.com>
2025-11-17 04:58:10 -0500

    GS7180 Add to the test scene

--- Assets/Scripts/Levels/Objects/KeyDoorTrigger.cs
+++ Assets/Scripts/Levels/Objects/KeyDoorTrigger.cs
@@ -1,8 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class KeyDoorTrigger : MonoBehaviour
 {
     [SerializeField] private AutoOpenDoor autoOpenDoor;
+    [SerializeField] private SpriteRenderer screenRenderer;
+    [SerializeField] private Sprite defaultSprite;
+    [SerializeField] private Sprite checkSprite;
+    [SerializeField] private Sprite crossSprite;
+
+    private Coroutine resetRoutine;
     void Start()
     {
         autoOpenDoor = transform.parent.GetComponentInChildren<AutoOpenDoor>();
@@ -13,6 +20,23 @@ public class KeyDoorTrigger : MonoBehaviour
         if (other.CompareTag("Player") && KeyOutlet.granted)
         {
             autoOpenDoor.OpenDoor();
+            screenRenderer.sprite = checkSprite;
+            return;
         }
+        else if (other.CompareTag("Player") && !KeyOutlet.granted)
+        {
+            screenRenderer.sprite = crossSprite;
+            if (resetRoutine != null)
+                StopCoroutine(resetRoutine);
+
+            resetRoutine = StartCoroutine(ResetAfterDelay());
+        }
+    }
+
+    private IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(5f);
+        screenRenderer.sprite = defaultSprite;
+        resetRoutine = null;
     }
 }
