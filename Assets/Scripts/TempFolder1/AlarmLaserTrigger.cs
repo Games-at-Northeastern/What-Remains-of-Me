fb8ae8d7661a01d1f48e55ca2b6cc5dedf386ce7
FF-Yan <biinxuans@gmail.com>
2025-11-17 14:34:49 -0500

    GS7194 Fix laser system

--- Assets/Scripts/TempFolder1/AlarmLaserTrigger.cs
+++ Assets/Scripts/TempFolder1/AlarmLaserTrigger.cs
@@ -5,15 +5,20 @@ public class AlarmLaserTrigger : MonoBehaviour, IAlarmListener
     [SerializeField] private DeathLaser laser;
     [SerializeField] private TimedToggle laserToggle;
     [SerializeField] private KeyOutlet keyOutlet;
+    [SerializeField] private bool isToggle = true;
 
     void Start()
     {
         keyOutlet.Subscribe(this);
-        laser.ToggleLaser();
+        if(laser)
+            laser.ToggleLaser();
     }
 
     public void OnAlarmStart()
     {
-        laserToggle.enabled = true;
+        if(laserToggle && isToggle)
+            laserToggle.enabled = true;
+        else if (laser)
+            laser.ToggleLaser();
     }
 }
