829d1a3116f36ff577db6caca9656282e2a05798
george <70286749+GeorgeEllis123@users.noreply.github.com>
2025-11-22 14:48:48 -0500

    GS-7248 (Add Horizontal Break Force to Vines in 1.2)
    The vines in 1.2 now have a horizontal force applied to them when they break

--- Assets/Scripts/Level Specific Scripts/Chapter 1/Level 2/VinesBreakEvent.cs
+++ Assets/Scripts/Level Specific Scripts/Chapter 1/Level 2/VinesBreakEvent.cs
@@ -2,6 +2,7 @@ using Ink.Parsed;
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 /// <summary>
 ///
@@ -13,12 +14,31 @@ public sealed class VinesBreakEvent : MonoBehaviour, IOnCollision
     [SerializeField]
     private UnityEvent<bool> ToDisableUponInvoke;
 
-    [SerializeField]
-    private List<GameObject> objs = new List<GameObject>();
+    [Tooltip("These objects will be deleted")]
+    [SerializeField] private List<GameObject> vineAnchors = new List<GameObject>();
+
+    [Tooltip("A random horizontal force will applied to these")]
+    [SerializeField] private List<GameObject> bonesToApplyForceTo = new List<GameObject>();
+
+    [Tooltip("The max the horizontal force that can be applied")]
+    [SerializeField] private float maxForce = 20f;
 
 
     [SerializeField]
     private bool _oneShot = true;
+
+    [Tooltip("FOR DEBUGGING WILL CAUSE THE TRIGGER EFFECT TO HAPPEN")]
+    [SerializeField] private bool testTrigger = false;
+
+    public void Update()
+    {
+        // This is here for Debugging purposes so you can easily test the trigger effect without going
+        // through all the steps of conducting it
+        if (testTrigger)
+        {
+           Collide(Vector2.zero);
+        }
+    }
 
     public void Collide(Vector2 direction)
     {
@@ -28,13 +48,23 @@ public sealed class VinesBreakEvent : MonoBehaviour, IOnCollision
 
         if (_oneShot)
         {
-            foreach (GameObject obj in objs)
+            foreach (GameObject obj in vineAnchors)
             {
                 if (obj != null)
                 {
                     Destroy(obj);
                 }
             }
+
+            foreach (GameObject obj in bonesToApplyForceTo)
+            {
+                var rb = obj.GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.AddForce(new Vector2(Random.Range(-maxForce, maxForce), 0), ForceMode2D.Impulse);
+                }
+            }
+            _oneShot = false;
         }
     }
 }
