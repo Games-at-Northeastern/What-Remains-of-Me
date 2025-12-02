5ef99b55895f9999d88630ea3a6a7e1583c4d41e
marcellalmiller <149106160+marcellalmiller@users.noreply.github.com>
2025-12-01 21:05:12 -0500

    Fix compiler errors

---
+++ Assets/Editor/CustomTilemapMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public static class CustomTilemapMenu
+{
+    [MenuItem("GameObject/2D Object/Tilemap/Custom", false, 10)]
+    public static void CreateCustomTilemap(MenuCommand menuCommand)
+    {
+        var grid = new GameObject("Grid", typeof(Grid));
+        GameObjectUtility.SetParentAndAlign(grid, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(grid, "Create Grid");
+
+        var tilemap = new GameObject("Custom Tilemap", typeof(Tilemap), typeof(TilemapRenderer), typeof(CustomTilemap));
+        tilemap.transform.SetParent(grid.transform, false);
+        Undo.RegisterCreatedObjectUndo(tilemap, "Create Custom Tilemap");
+
+        Selection.activeObject = tilemap;
+    }
+}
