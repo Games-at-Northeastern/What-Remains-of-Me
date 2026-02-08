981a0721c9edb1bf997d09ecd012778ba9690539
Em-Ott <emilymott2023@gmail.com>
2026-02-08 15:15:27 -0500

    GS-7643 Added basic menu
    Added basic menu + put reset all progress text in there.
    Task should now be completed aside from testing + QA script.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -11,6 +11,7 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private int sceneID;
+    [SerializeField] private GameObject optionsMenu; 
 
    /// <summary>
    /// Loads Chapter 1 Room 1 (the tutorial scene).
@@ -27,6 +28,16 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quitting Game");
         Application.Quit();
+    }
+
+    public void ShowMenu()
+    {
+        optionsMenu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        optionsMenu.SetActive(false);
     }
 
 
