fddf49554207768eea27d865502e2c1235735f28
Sorano7 <sorano27@outlook.com>
2026-03-23 20:32:00 -0400

    GS-5722 Generalize 1.5.2 bottom puzzle door animation script
    Add a script for 1.5.2 bottom puzzle sliding doors that can be opened
    (with animation).

---
+++ Assets/Scripts/Level Specific Scripts/Chapter 1/Level 5/VoxDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Represents a horizontal sliding door for the Vox bottom puzzle entrance.
+/// Requires an Animator with a bool parameter named "Opening".
+/// </summary>
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider2D))]
+public class VoxDoor : MonoBehaviour
+{
+    private Animator _anim;
+    private Collider2D _col;
+
+    private void Start()
+    {
+        _anim.SetBool("Opening", false);
+        _col.enabled = true;
+    }
+
+    /// <summary>
+    /// Open this door and disable its collider.
+    /// </summary>
+    public void Open()
+    {
+        _anim.SetBool("Opening", true);
+        _col.enabled = false;
+    }
+}
