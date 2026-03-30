fb794cba1e6b3c41e04fab17d72184532454d37b
Sorano7 <sorano27@outlook.com>
2026-03-30 16:00:01 -0400

    GS-5604 Implement Cutscene for Vox's bottom outlet opening
    Implement a cutscene that highlights the outlet to the bottom puzzle
    behind the triple doors when it is unlocked.
    VoxFirstOutletCutsceneTrigger.cs handles playback when the condition is
    met and VoxFirstOutletCutscene.playable is the cutscene timeline.

---
+++ Assets/Scripts/Level Specific Scripts/Chapter 1/Level 5/VoxFirstOutletCutsceneTrigger.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PlayerController;
+using UnityEngine;
+using UnityEngine.Playables;
+
+/// <summary>
+/// Controls the cutscene playback when the first outlet is unlocked to the player.
+/// </summary>
+public class VoxFirstOutletCutsceneTrigger : MonoBehaviour
+{
+    [Header("Cutscene")]
+    [SerializeField] private PlayableDirector cutscene;
+    
+    [Header("Condition")]
+    [SerializeField] private List<AControllable> doors;
+
+    private PlayerController2D _player;
+    
+    private bool _hasPlayed;
+
+    private void Start()
+    {
+        cutscene.playOnAwake = false;
+        cutscene.stopped += OnCutsceneStopped;
+        _player = FindFirstObjectByType<PlayerController2D>();
+    }
+
+    private void Update()
+    {
+        if (!_hasPlayed && ShouldPlay()) PlayCutscene();
+    }
+    
+    /// <summary>
+    /// Returns whether the cutscene should play.
+    /// </summary>
+    /// <returns></returns>
+    private bool ShouldPlay()
+    {
+        return doors.All(d => Mathf.Approximately(d.GetPercentFull(), 0f));
+    }
+
+    /// <summary>
+    /// Play the cutscene and lock player control.
+    /// </summary>
+    private void PlayCutscene()
+    {
+        _hasPlayed = true;
+        _player.LockInputs();
+        cutscene.Play();
+    }
+
+    /// <summary>
+    /// Restore control and disable gameobject when the cutscene is stopped.
+    /// </summary>
+    /// <param name="_"></param>
+    private void OnCutsceneStopped(PlayableDirector _)
+    {
+        _player.UnlockInputs();
+        gameObject.SetActive(false);
+    }
+}
