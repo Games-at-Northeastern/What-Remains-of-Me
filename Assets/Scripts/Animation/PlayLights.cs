af120330bf5c6af0e05fbb53d9964589df212053
Ryder Schmelzle <skilledshadow5000@gmail.com>
2026-04-07 18:41:17 -0400

    Fixed Bug With Lights and Death Laser Timing. Also, Ensured the 1.5.1 death pits look better

--- Assets/Scripts/Animation/PlayLights.cs
+++ Assets/Scripts/Animation/PlayLights.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
-
 public class PlayLights : Effects
 {
-    [SerializeField] Light2D[] lights;
+    [SerializeField]
+    private Light2D[] lights;
+
+    private void OnEnable() => lights = lights.Where(item => item != null).ToArray();
+
     public override void PlayEffect()
     {
-        foreach (Light2D light in lights)
-        {
+        foreach (Light2D light in lights) {
             light.enabled = true;
         }
 
@@ -17,8 +18,7 @@ public class PlayLights : Effects
 
     public override void CancelEffect()
     {
-        foreach (Light2D light in lights)
-        {
+        foreach (Light2D light in lights) {
             light.enabled = false;
         }
     }
