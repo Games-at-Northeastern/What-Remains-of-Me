305af8ba5af23134232d639f2729cce26e8f2127
Sorano7 <sorano27@outlook.com>
2026-04-13 15:03:53 -0400

    GS-8029 Implement ScriptAllowing Vox to Get Damaged After Each Puzzle
    Add VoxDamageOverlayController.cs that updates an overlay after each
    puzzle completion to make Vox appear more damaged.
    
    a

---
+++ Assets/Scripts/Level Specific Scripts/Chapter 1/Level 5/VoxDamageOverlayController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Handles making Vox appear more damaged visually after each puzzle.
+/// Expects an animator for the overlay with an integer parameter named "DamageLevel".
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class VoxDamageOverlayController : MonoBehaviour
+{
+    /// <summary>
+    /// Represents a reference to one of the four puzzles, used to evaluate completion.
+    /// </summary>
+    [Serializable]
+    public struct Puzzle
+    {
+        public AControllable controllable;
+        [Range(0f, 1f)] public float expectedCharge;
+    }
+    
+    [SerializeField] private List<Puzzle> puzzles;
+
+    private Animator _anim;
+    private int _prevCount;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Skip update if the damage level is already at maximum.
+        if (_prevCount == puzzles.Count) return;
+        
+        // Filter the puzzle doors by opened state (which correspond to puzzle completion)
+        var count = puzzles.Where(IsCompleted).ToList().Count;
+        
+        // If the new count is lower than highest seen, do not revert the damage state.
+        if (count <= _prevCount) return;
+
+        // Set the animator parameter
+        _anim.SetInteger("DamageLevel", count);
+        _prevCount = count;
+    }
+
+    /// <summary>
+    /// Returns whether a puzzle is completed.
+    /// </summary>
+    /// <param name="puzzle">the target puzzle</param>
+    /// <returns></returns>
+    private static bool IsCompleted(Puzzle puzzle)
+    {
+        return Mathf.Approximately(puzzle.controllable.GetPercentFull(), puzzle.expectedCharge);
+    }
+}
